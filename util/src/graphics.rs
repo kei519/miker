3bcd9e818711bfac4ed3dae6ae699ffa064d826d
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-07-10 23:46:45 +0900

    Add GlayscalePrint

--- util/src/graphics.rs
+++ util/src/graphics.rs
@@ -1,3 +1,5 @@
+use crate::font_data;
+
 /// A trait for painting pixels with grayscale.
 pub trait GlayscalePixelWrite {
     /// Paints `pos` with `color` if `pos` is in the range.
@@ -13,3 +15,28 @@ pub trait GlayscalePixelWrite {
     /// Returns writable area (width, height).
     fn range(&self) -> (usize, usize);
 }
+
+/// A trait for printing string with grayscale color.
+pub trait GlayscalePrint: GlayscalePixelWrite {
+    /// Prints `string` at `pos` (x, y).
+    fn print(&mut self, string: &str, pos: (usize, usize)) {
+        let (width, height) = self.range();
+        let col_num = width / 8;
+        let row_num = height / 16;
+        for (row, line) in string.lines().enumerate().take(row_num) {
+            for (col, c) in line.chars().enumerate().take(col_num) {
+                for (y, &bit) in font_data::get_font(c as _).iter().enumerate() {
+                    for (x, color) in (0..8)
+                        .map(|i| if bit >> (7 - i) & 1 != 0 { 0xff } else { 0x00 })
+                        .enumerate()
+                    {
+                        let pos = (pos.0 + col * 8 + x, pos.1 + row * 16 + y);
+                        self.write(pos, color);
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl<T: GlayscalePixelWrite> GlayscalePrint for T {}
