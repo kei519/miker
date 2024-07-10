999828f011b97ac0144358557c85a04dc1c32b2e
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-07-10 14:08:35 +0900

    Add struct representing frame buffer info

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -74,3 +74,20 @@ pub enum ElfProgType {
     GnuProperty = 0x6474_e553,
     GnuStack = Self::Loos as u32 + 0x0474_e551,
 }
+
+#[derive(Debug, Clone)]
+pub struct FrameBufferInfo {
+    pub format: PixelFormat,
+    pub horizontal_resolution: usize,
+    pub vertical_resolution: usize,
+    pub pixels_per_scanline: usize,
+    pub frame_buffer: usize,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum PixelFormat {
+    Rgb,
+    Bgr,
+    Bitmask,
+    Bitonly,
+}
