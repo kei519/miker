a2c652c95e05e3b98520aac1968db1434e9f8bd9
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-07-11 00:48:01 +0900

    Display memmap in 2nd loader

--- loader2/src/main.rs
+++ loader2/src/main.rs
@@ -1,14 +1,17 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Write as _;
+
 use uefi::table::boot::MemoryMap;
 use util::{
+    buffer::StrBuf,
     graphics::{GlayscalePixelWrite as _, GlayscalePrint as _},
     screen::{FrameBufferInfo, GlayscaleScreen},
 };
 
 #[no_mangle]
-fn _start(fb_info: &FrameBufferInfo, _: &MemoryMap) {
+fn _start(fb_info: &FrameBufferInfo, memmap: &MemoryMap) {
     let mut screen = GlayscaleScreen::new(fb_info.clone());
     // Draw whole screen with gray.
     let size = screen.range();
@@ -18,7 +21,28 @@ fn _start(fb_info: &FrameBufferInfo, _: &MemoryMap) {
         }
     }
 
-    screen.print("Hello 2nd loader!", (0, 0));
+    // Display memmap
+    let mut buf = [0; 256];
+    // 1 memory descritpor length in display.
+    let item_len = 60;
+
+    let col_num = screen.range().0 / 8;
+    let row_num = screen.range().1 / 16;
+    let item_per_row = col_num / item_len;
+    for (count, desc) in memmap.entries().enumerate().take(row_num * item_per_row) {
+        let mut buf = StrBuf::new(&mut buf);
+        let _ = write!(
+            buf,
+            "{:016x}-{:016x}: {:?}",
+            desc.phys_start,
+            desc.phys_start + desc.page_count * 4096,
+            desc.ty,
+        );
+        // Calculate start position.
+        let col = count / row_num;
+        let row = count % row_num;
+        screen.print(buf.to_str(), (col * item_len * 8, row * 16));
+    }
 
     loop {
         unsafe { core::arch::asm!("hlt") };
