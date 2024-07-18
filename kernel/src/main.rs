0adfd53227b177b9ab4fea66aad7628bea1ef1d0
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-07-18 16:08:48 +0900

    Show memmap debug info in kernel

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,9 +3,9 @@
 
 use core::fmt::Write as _;
 
-use uefi::table::{boot::MemoryMap, cfg::ACPI2_GUID, Runtime, SystemTable};
+use kernel::memmap::PAGE_MAP;
+use uefi::table::{boot::MemoryMap, Runtime, SystemTable};
 use util::{
-    acpi::Rsdp,
     asmfunc,
     buffer::StrBuf,
     graphics::GrayscalePrint as _,
@@ -32,39 +32,22 @@ _start:
 }
 
 #[no_mangle]
-fn main(fb_info: &FrameBufferInfo, _memmap: &'static MemoryMap, runtime: SystemTable<Runtime>) {
+fn main(fb_info: &FrameBufferInfo, memmap: &'static MemoryMap, _runtime: SystemTable<Runtime>) {
     let mut screen = GrayscaleScreen::new(fb_info.clone());
     FB_INFO.init(fb_info.clone());
+    // Safety: There is one processor running and this is the first time to initialize.
+    unsafe { PAGE_MAP.init(memmap) };
 
-    let mut buf = [0; 4096];
-
-    let rsdp_ptr = runtime
-        .config_table()
-        .iter()
-        .find_map(|config| {
-            if config.guid == ACPI2_GUID {
-                Some(config.address)
-            } else {
-                None
-            }
-        })
-        .unwrap_or_else(|| panic!("There is no ACPI2 entry."));
-    // Safety: UEFI pass the proper one.
-    let rsdp = unsafe { Rsdp::from_ptr(rsdp_ptr.cast()).unwrap() };
-
-    // let entry = rsdp.xsdt().unwrap().entry(0).unwrap();
+    let mut buf = [0; 4 * 4096];
     let mut buf = StrBuf::new(&mut buf);
-    let _ = writeln!(buf, "RSDT entries:");
-    for entry in rsdp.rsdt().unwrap().entries() {
-        let _ = writeln!(buf, "{:?}", entry);
+    let _ = write!(buf, "{:#x?}", PAGE_MAP);
+    let num_row = FB_INFO.as_ref().vertical_resolution / 16;
+    let col_len = 30;
+    for (i, line) in buf.to_str().lines().enumerate() {
+        let col = i / num_row;
+        let row = i % num_row;
+        screen.print(line, (col * col_len * 8, row * 16));
     }
-    let _ = writeln!(buf);
-
-    let _ = writeln!(buf, "XSDT entries:");
-    for entry in rsdp.xsdt().unwrap().entries() {
-        let _ = writeln!(buf, "{:?}", entry);
-    }
-
     screen.print(buf.to_str(), (0, 0));
 
     loop {
