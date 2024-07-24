7dd665d419305cadef0908ad4cc205840ef7554b
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-07-24 21:37:37 +0900

    Deny improper ctypes
    To avoid not FFI safe calling.

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -3,6 +3,8 @@
 #![cfg_attr(not(test), no_std)]
 #![deny(unsafe_op_in_unsafe_fn)]
 #![deny(missing_docs)]
+#![deny(improper_ctypes)]
+#![deny(improper_ctypes_definitions)]
 
 pub mod memmap;
 pub mod screen;
