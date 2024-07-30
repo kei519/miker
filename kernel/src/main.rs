da058b5bef34cd00528377fa30842a896246502c
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-07-30 11:56:34 +0900

    Remove memory allocation tests in kernel::main2

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -12,8 +12,7 @@ mod task;
 
 use core::fmt::Write as _;
 
-use alloc::{boxed::Box, vec::Vec};
-use alloc::{format, vec};
+use alloc::format;
 use task::TASK_MANAGER;
 use uefi::table::{boot::MemoryMap, Runtime, SystemTable};
 use util::apic;
@@ -79,8 +78,6 @@ fn main(fb_info: &FrameBufferInfo, memmap: &'static mut MemoryMap, runtime: Syst
 
 // NOTE: Never return `Ok()`.
 fn main2(_runtime: SystemTable<Runtime>) -> Result<()> {
-    let mut screen = GrayscaleScreen::new(FB_INFO.as_ref().clone());
-
     let stack_for_timer_interrupt = PAGE_MAP.allocate(2);
     if stack_for_timer_interrupt.is_null() {
         panic!("Failed to allocate 2 pages");
@@ -104,113 +101,6 @@ fn main2(_runtime: SystemTable<Runtime>) -> Result<()> {
 
     interrupt::init()?;
 
-    let mut buf = [0; 4 * 4096];
-    let mut buf = StrBuf::new(&mut buf);
-
-    let num_free_pages = PAGE_MAP.free_pages_count();
-    let _ = writeln!(buf, "Free: {} pages", num_free_pages);
-    let _ = writeln!(buf);
-
-    // Many allocations.
-    let mut starts2 = [[core::ptr::null_mut(); 11]; 20];
-    for starts in &mut starts2 {
-        for (order, start) in starts.iter_mut().enumerate() {
-            *start = PAGE_MAP.allocate(1 << order);
-            assert!(!start.is_null());
-        }
-    }
-    let mut starts = [core::ptr::null_mut(); 11];
-    for (order, start) in starts.iter_mut().enumerate() {
-        *start = PAGE_MAP.allocate(1 << order);
-    }
-    for (order, start) in starts.into_iter().enumerate() {
-        unsafe { PAGE_MAP.free(start, 1 << order) };
-    }
-
-    for starts in starts2 {
-        for (order, start) in starts.into_iter().enumerate() {
-            unsafe { PAGE_MAP.free(start, 1 << order) };
-        }
-    }
-
-    let _ = writeln!(buf, "Large allocation");
-    {
-        let mut v = vec![94; 1028];
-        let _ = writeln!(buf, "v: {:p}, cap: {}", v.as_ptr(), v.capacity());
-        v.extend_from_slice(&[1024; 10]);
-        let _ = writeln!(buf, "v: {:p}, cap: {}", v.as_ptr(), v.capacity());
-        v.extend_from_slice(&[512; 10]);
-        let _ = writeln!(buf, "v: {:p}, cap: {}", v.as_ptr(), v.capacity());
-        let _ = writeln!(buf);
-        for (i, item) in v.into_iter().enumerate() {
-            match i {
-                0..=1027 => assert_eq!(item, 94),
-                1028..=1037 => assert_eq!(item, 1024),
-                1038..=1047 => assert_eq!(item, 512),
-                _ => unreachable!(),
-            }
-        }
-    }
-
-    let _ = writeln!(buf, "First small some allocations");
-    {
-        let mut v = vec![0; 16];
-        let _ = writeln!(buf, "v: {:p}, cap: {}", v.as_ptr(), v.capacity());
-        v.extend_from_slice(&[1024; 16]);
-        let _ = writeln!(buf, "v: {:p}, cap: {}", v.as_ptr(), v.capacity());
-        let us: Vec<_> = (0..10).map(|_| v.clone()).collect();
-        for (i, u) in us.iter().enumerate() {
-            let _ = writeln!(buf, "u{i}: {:p}, cap: {}", u.as_ptr(), u.capacity());
-        }
-        let _ = writeln!(buf);
-    }
-
-    let _ = writeln!(buf, "Second some small and large allocations");
-    {
-        let mut v = vec![0i8; 16];
-        let _ = writeln!(buf, "v: {:p}, cap: {}", v.as_ptr(), v.capacity());
-        v.extend_from_slice(&[i8::MIN; 16]);
-        let _ = writeln!(buf, "v: {:p}, cap: {}", v.as_ptr(), v.capacity());
-        let us: Vec<_> = (0..10)
-            .map(|_| {
-                let hoge = vec![0; 64];
-                core::hint::black_box(&hoge);
-                v.clone()
-            })
-            .collect();
-        for (i, u) in us.iter().enumerate() {
-            let _ = writeln!(buf, "u{i}: {:p}, cap: {}", u.as_ptr(), u.capacity());
-        }
-        let _ = writeln!(buf);
-    }
-
-    let _ = writeln!(buf, "Third some small allocations");
-    {
-        let mut v = vec![0i8; 16];
-        let _ = writeln!(buf, "v: {:p}, cap: {}", v.as_ptr(), v.capacity());
-        v.extend_from_slice(&[i8::MIN; 16]);
-        let _ = writeln!(buf, "v: {:p}, cap: {}", v.as_ptr(), v.capacity());
-        let us: Vec<_> = (0..10).map(|_| v.clone()).collect();
-        for (i, u) in us.iter().enumerate() {
-            let _ = writeln!(buf, "u{i}: {:p}, cap: {}", u.as_ptr(), u.capacity());
-        }
-        let _ = writeln!(buf);
-    }
-
-    let _ = writeln!(buf, "Some box allocations");
-    {
-        let b = Box::new(0u8);
-        let _ = writeln!(buf, "b: {:p}", Box::into_raw(b));
-        let us: Vec<_> = (0..10).map(|_| Box::new(12u16)).collect();
-        for (i, b) in us.into_iter().enumerate() {
-            let _ = writeln!(buf, "b{i}: {:p}", Box::into_raw(b));
-        }
-        let _ = writeln!(buf);
-    }
-
-    let _ = writeln!(buf, "Free: {} pages", PAGE_MAP.free_pages_count());
-    screen.print(buf.to_str(), (0, 0));
-
     TASK_MANAGER.init();
 
     apic::set_lvt_timer(0x40, false, true);
@@ -218,7 +108,6 @@ fn main2(_runtime: SystemTable<Runtime>) -> Result<()> {
     apic::set_init_count(100000);
     asmfunc::sti();
 
-    drop(screen);
     TASK_MANAGER.register_new_task(count, 1, 1 << 3, 2 << 3);
 
     loop {
